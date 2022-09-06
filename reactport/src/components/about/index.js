fe67b14fc398979856856cfe996e17aa5172e2f1
austin green <trentgr33n@icloud.com>
2022-09-05 20:11:24 -0400

    added components

---
+++ reactport/src/components/about/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function About() {
+
+    return(
+        <section className="container">
+            <h2 class="top-title">Austin Green</h2>
+            <hr></hr>
+            <div>
+            <p>
+                Full-Stack Deveoper studying at the University of Vanderbilt.
+            </p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
